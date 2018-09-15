10a20b88cec2ea455b2f8dadd09d5e76913f7a3a
bergeramit <bergeramit@gmail.com>
2018-09-15 20:55:51 +0300

    fixed ugly random problem

--- vectors_util.py
+++ vectors_util.py
@@ -20,7 +20,7 @@ def update_position(current_position, current_velocity, time_elapsed):
 
 
 def get_random_direction_vector():
-    return Vector((random.random() - 0.5) * 2, (random.random() - 0.5) * 2)
+    return Vector(random.uniform(-1, 1), random.uniform(-1, 1))
 
 
 class Vector:
